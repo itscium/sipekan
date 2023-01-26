7f66e075ee9c138624018cdcc9d94bae226b08b9
baluhap <raminsonsiregar@gmail.com>
2023-01-26 10:33:57 +0700

    change setting into personal identity on profile

--- resources/views/personal/profile/index.blade.php
+++ resources/views/personal/profile/index.blade.php
@@ -81,9 +81,9 @@
                 <div class="card">
                     <div class="card-header p-2">
                         <ul class="nav nav-pills">
-{{--                            <li class="nav-item"><a class="nav-link" href="#settings" data-toggle="tab">Settings</a></li>--}}
+                            <li class="nav-item"><a class="nav-link active" href="#identity" data-toggle="tab">Personal Identity</a></li>
                             @if($cek !== null)
-                            <li class="nav-item"><a class="nav-link active" href="#salary" data-toggle="tab">Salary & Allowances</a></li>
+                            <li class="nav-item"><a class="nav-link" href="#salary" data-toggle="tab">Salary & Allowances</a></li>
                             @can('head_dept')
                                 <li class="nav-item"><a class="nav-link" href="#departemen" data-toggle="tab">Departement Budget</a></li>
                             @endcan
@@ -92,55 +92,29 @@
                     </div><!-- /.card-header -->
                     <div class="card-body">
                         <div class="tab-content">
-{{--                            <div class="tab-pane" id="settings">--}}
-{{--                                <form class="form-horizontal">--}}
-{{--                                    <div class="form-group row">--}}
-{{--                                        <label for="inputName" class="col-sm-2 col-form-label">Name</label>--}}
-{{--                                        <div class="col-sm-10">--}}
-{{--                                            <input type="email" class="form-control" id="inputName" placeholder="Name">--}}
-{{--                                        </div>--}}
-{{--                                    </div>--}}
-{{--                                    <div class="form-group row">--}}
-{{--                                        <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>--}}
-{{--                                        <div class="col-sm-10">--}}
-{{--                                            <input type="email" class="form-control" id="inputEmail" placeholder="Email">--}}
-{{--                                        </div>--}}
-{{--                                    </div>--}}
-{{--                                    <div class="form-group row">--}}
-{{--                                        <label for="inputName2" class="col-sm-2 col-form-label">Name</label>--}}
-{{--                                        <div class="col-sm-10">--}}
-{{--                                            <input type="text" class="form-control" id="inputName2" placeholder="Name">--}}
-{{--                                        </div>--}}
-{{--                                    </div>--}}
-{{--                                    <div class="form-group row">--}}
-{{--                                        <label for="inputExperience" class="col-sm-2 col-form-label">Experience</label>--}}
-{{--                                        <div class="col-sm-10">--}}
-{{--                                            <textarea class="form-control" id="inputExperience" placeholder="Experience"></textarea>--}}
-{{--                                        </div>--}}
-{{--                                    </div>--}}
-{{--                                    <div class="form-group row">--}}
-{{--                                        <label for="inputSkills" class="col-sm-2 col-form-label">Skills</label>--}}
-{{--                                        <div class="col-sm-10">--}}
-{{--                                            <input type="text" class="form-control" id="inputSkills" placeholder="Skills">--}}
-{{--                                        </div>--}}
-{{--                                    </div>--}}
-{{--                                    <div class="form-group row">--}}
-{{--                                        <div class="offset-sm-2 col-sm-10">--}}
-{{--                                            <div class="checkbox">--}}
-{{--                                                <label>--}}
-{{--                                                    <input type="checkbox"> I agree to the <a href="#">terms and conditions</a>--}}
-{{--                                                </label>--}}
-{{--                                            </div>--}}
-{{--                                        </div>--}}
-{{--                                    </div>--}}
-{{--                                    <div class="form-group row">--}}
-{{--                                        <div class="offset-sm-2 col-sm-10">--}}
-{{--                                            <button type="submit" class="btn btn-danger">Submit</button>--}}
-{{--                                        </div>--}}
-{{--                                    </div>--}}
-{{--                                </form>--}}
-{{--                            </div>--}}
-                            <div class="tab-pane active" id="salary">
+                            <div class="tab-pane active" id="identity">
+                                <form class="form-horizontal">
+                                    <div class="form-group row">
+                                        <label for="inputName" class="col-sm-2 col-form-label">Name</label>
+                                        <div class="col-sm-10">
+                                            <input type="email" class="form-control" id="inputName" value="{{$user->name}}" disabled>
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
+                                        <div class="col-sm-10">
+                                            <input type="email" class="form-control" id="inputEmail" value="{{$user->email}}" disabled>
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label for="inputEmail" class="col-sm-2 col-form-label">Departemen</label>
+                                        <div class="col-sm-10">
+                                            <input type="email" class="form-control" id="inputEmail" value="{{$user->kepala_departemen->nama_departemen ?? ''}}" disabled>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                            <div class="tab-pane" id="salary">
                                 <div class="row">
                                     <div class="col-md-6">
                                         <fieldset class="border p-2 mb-2">
